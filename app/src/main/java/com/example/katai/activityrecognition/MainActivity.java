44706706792a021943cb010e19625a1525de5f01
Ka Tai Ho <kataihoo@gmail.com>
2017-11-03 23:23:03 -0700

    first commit

---
+++ app/src/main/java/com/example/katai/activityrecognition/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.katai.activityrecognition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Text
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
